09a5d9a79e92bc5d1943cc302826bf7bd2fcfe33
Surajdusane <138127406+Surajdusane@users.noreply.github.com>
2025-02-10 22:25:04 +0530

    datafast

---
+++ app/_document.tsx
@@ -0,0 +1,19 @@
+// In your page or component, for example, pages/index.js
+
+import Head from 'next/head';
+
+const HomePage = () => (
+  <>
+    <Head>
+      <script
+        defer
+        data-website-id="67aa2e356359da8abd47c088"
+        data-domain="finyyy.vercel.app"
+        src="https://datafa.st/js/script.js"
+      ></script>
+    </Head>
+    <h1>Welcome to the Home Page</h1>
+  </>
+);
+
+export default HomePage;
